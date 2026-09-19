ee8622e284fef1ad81ba87c44bfe63b95335e6a5
MateoEGomez <Gomezmateoezequiel@gmail.com>
2026-09-19 04:36:50 -0300

    Metodos modificar y eliminar en marcanegocio

--- negocio/MarcaNegocio.cs
+++ negocio/MarcaNegocio.cs
@@ -22,5 +22,44 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        public void modificar(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Update MARCAS set Descripcion = @descripcion Where Id = @id");
+                datos.setearParametro("@descripcion", marca.Descripcion);
+                datos.setearParametro("@id", marca.Id);
+                datos.ejecutarAccion();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Delete from MARCAS where Id = @id");
+                datos.setearParametro("@id", id);
+                datos.ejecutarAccion();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
