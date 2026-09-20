6965033a60fa07c8501753c4a091193fd9c61afa
MateoEGomez <Gomezmateoezequiel@gmail.com>
2026-09-20 20:52:04 -0300

    Front de menu principal

---
+++ ui/FrmMenuPrincipal.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ui
+{
+    partial class FrmMenuPrincipal
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnArticulos = new System.Windows.Forms.Button();
+            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnCategorias = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // lblTitulo
+            //
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(12, 15);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(276, 35);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Gestión de catálogo";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // btnArticulos
+            //
+            this.btnArticulos.Location = new System.Drawing.Point(40, 70);
+            this.btnArticulos.Name = "btnArticulos";
+            this.btnArticulos.Size = new System.Drawing.Size(220, 40);
+            this.btnArticulos.TabIndex = 1;
+            this.btnArticulos.Text = "Artículos";
+            this.btnArticulos.UseVisualStyleBackColor = true;
+            this.btnArticulos.Click += new System.EventHandler(this.btnArticulos_Click);
+            //
+            // btnMarcas
+            //
+            this.btnMarcas.Location = new System.Drawing.Point(40, 120);
+            this.btnMarcas.Name = "btnMarcas";
+            this.btnMarcas.Size = new System.Drawing.Size(220, 40);
+            this.btnMarcas.TabIndex = 2;
+            this.btnMarcas.Text = "Marcas";
+            this.btnMarcas.UseVisualStyleBackColor = true;
+            this.btnMarcas.Click += new System.EventHandler(this.btnMarcas_Click);
+            //
+            // btnCategorias
+            //
+            this.btnCategorias.Location = new System.Drawing.Point(40, 170);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Size = new System.Drawing.Size(220, 40);
+            this.btnCategorias.TabIndex = 3;
+            this.btnCategorias.Text = "Categorías";
+            this.btnCategorias.UseVisualStyleBackColor = true;
+            this.btnCategorias.Click += new System.EventHandler(this.btnCategorias_Click);
+            //
+            // btnSalir
+            //
+            this.btnSalir.Location = new System.Drawing.Point(40, 235);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(220, 35);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            //
+            // FrmMenuPrincipal
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 290);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCategorias);
+            this.Controls.Add(this.btnMarcas);
+            this.Controls.Add(this.btnArticulos);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP WinForm - Equipo F";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnArticulos;
+        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnCategorias;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ ui/FrmMenuPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ui
+{
+    public partial class FrmMenuPrincipal : Form
+    {
+        public FrmMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArticulos_Click(object sender, EventArgs e)
+        {
+            Form1 articulos = new Form1();
+            articulos.ShowDialog();
+        }
+
+        private void btnMarcas_Click(object sender, EventArgs e)
+        {
+            FrmMarcas marcas = new FrmMarcas();
+            marcas.ShowDialog();
+        }
+
+        private void btnCategorias_Click(object sender, EventArgs e)
+        {
+            FrmCategorias categorias = new FrmCategorias();
+            categorias.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
