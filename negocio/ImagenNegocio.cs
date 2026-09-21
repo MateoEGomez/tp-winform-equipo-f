893b6f46b550b9b3a9e789d18a2736bc6d44ae1b
MateoEGomez <Gomezmateoezequiel@gmail.com>
2026-09-20 22:04:36 -0300

    Modificiacion en listar + eliminacion de imagenes

--- negocio/ImagenNegocio.cs
+++ negocio/ImagenNegocio.cs
@@ -1,9 +1,42 @@
+using System.Collections.Generic;
 using dominio;
 
 namespace negocio
 {
     public class ImagenNegocio
     {
+        public List<Imagen> listar()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, IdArticulo, ImagenUrl From IMAGENES");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
         public void agregar(Imagen nueva)
         {
             AccesoDatos datos = new AccesoDatos();
@@ -12,6 +45,25 @@ namespace negocio
                 datos.setearConsulta("Insert into IMAGENES (IdArticulo, ImagenUrl) values (@idArticulo, @url)");
                 datos.setearParametro("@idArticulo", nueva.IdArticulo);
                 datos.setearParametro("@url", nueva.ImagenUrl);
+                datos.ejecutarAccion();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminarPorArticulo(int idArticulo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Delete from IMAGENES where IdArticulo = @idArticulo");
+                datos.setearParametro("@idArticulo", idArticulo);
                 datos.ejecutarAccion();
             }
             catch (System.Exception ex)
